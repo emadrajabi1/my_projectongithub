e3c414313b4aad7666be49bf695005225bebee1e
Emad Rajabi <emad.rajabi1@gmail.com>
2026-04-21 19:52:03 -0600

    improve hex

--- Hex_output/hex_output.py
+++ Hex_output/hex_output.py
@@ -1,20 +1,33 @@
 #Hex_output
 def hex_output():
-    dec_value = 0
-    hex_string = input('Enter a hex number:')
+    while True:
+        hex_string = input('Enter a hex number:')
     
-    for power , digit in enumerate(reversed(hex_string)):
-        if '0' <= digit <= '9':
-            num = int(digit)
-        elif 'a' <= digit.lower() <= 'f':
-            num = ord(digit.lower()) - 87
-        else:
-            print('invalid hex digit!')
-            return
+        if hex_string == '':
+            print('Goodbye!')
+            break
         
-        dec_value +=num*(16**power)
+        print(f'Your input: {hex_string}')
+    
+        dec_value = 0    
+        valid_input = True
+    
+        for power , digit in enumerate(reversed(hex_string)):
+            if '0' <= digit <= '9':
+                num = int(digit)
+            elif 'a' <= digit.lower() <= 'f':
+                num = ord(digit.lower()) - 87
+            else:
+                print('invalid hex digit!')
+                valid_input = False
+                break
+            dec_value +=num*(16**power)
         
-    print(dec_value)
-      
+        if valid_input:
+            print(f'Decimal value: {dec_value}')
+        
+            print('-' * 30)
+    
+    
 hex_output()
 
