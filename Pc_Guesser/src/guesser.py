e522a72b0c171fbc660afe4c364b7e380ac105a6
Emad Rajabi <emad.rajabi1@gmail.com>
2026-08-13 17:18:39 -0600

    Adding new feature to code

--- Pc_Guesser/src/guesser.py
+++ Pc_Guesser/src/guesser.py
@@ -18,17 +18,25 @@ def main() -> None:
     high: int = 1000
     guess: int = 0
     feedback: str = ""
-
+    max_attempts = 8
+    attempt = 0
+    
     print("Think of a number between 1 and 1000.")
     print("I'll try to guess it!\n")
+     
 
     while feedback != 'c':
         # Binary search: calculate middle point
         guess = (low + high) // 2
+        attempt +=1
         
         feedback = input(
             f"Is {guess} your number? (c = correct, b = bigger, s = smaller): "
         ).strip().lower()
+        
+        if feedback == 'q' or feedback == '' or feedback == '\x1b' :
+            print('\n Game exited by user.Goodbye!')
+            break
 
         if feedback == 'b':
             low = guess + 1
@@ -36,9 +44,15 @@ def main() -> None:
             high = guess - 1
         elif feedback == 'c':
             print(f"\n🎉 I guessed it! The number is {guess}")
+            score = (max_attempts - attempt + 1)*10
+            print('Score:',score)
             break
         else:
             print("Invalid input! Please use 'c', 'b', or 's'.")
+            
+        if attempt >= max_attempts and feedback != 'c':
+            print('\n Game Over! You Win , PC is a Loser')
+            break
 
     print("Thanks for playing!")
 
